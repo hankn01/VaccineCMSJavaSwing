4db2ca75590f34ee5c638071df5734289e7ce326
kanei0415 <kimyoungho0415@gmail.com>
2021-05-15 21:34:30 +0900

    use JTable

--- vaccineView/EffectivenessContents.java
+++ vaccineView/EffectivenessContents.java
@@ -1,16 +1,13 @@
 package vaccineView;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
 import javax.swing.*;
 
-import vaccine.Inoculationable;
 import vaccine.Vaccine;
 
-public class EffectivenessContents extends JPanel
-implements Inoculationable{
+public class EffectivenessContents extends JPanel {
 	public static final int INPUT_MAX_SIZE = 30;
+	
+	public static final String[] HEADER = {"Age", "Risk(%)", "Effectiveness(%)", "Inoculationable"};
 	
 	private double[] risks = {
 			0,0,0,0,0,0,0,0,0,0
@@ -20,72 +17,25 @@ implements Inoculationable{
 			0,0,0,0,0,0,0,0,0,0
 	};
 	
+	private String[][] body = new String[risks.length][HEADER.length];
+	
 	private Vaccine vaccine;
 	
 	public EffectivenessContents(Vaccine vaccine) {
 		super();
-		
 		this.vaccine = vaccine;
 		
-		setLayout(new BorderLayout());
-		
-		JPanel riskContents = new JPanel();
-		riskContents.setLayout(new GridLayout(risks.length, 1));
-		
 		for(int i=0 ; i<risks.length ; i++) {
-			riskContents.add(new JLabel((i*10) + " ~ " + ((i+1)*10-1)  + " ages : " + risks[i] + "% " + effectiveness[i] + "% " + ((risks[i]>effectiveness[i])?"RISKY":"EFFECTIVE")));
+			body[i][0] = (i*10) + " ~ " + ((i+1)*10-1);
+			body[i][1] = risks[i] + "";
+			body[i][2] = effectiveness[i] + "";
+			body[i][3] = (risks[i]>effectiveness[i]) ? "RISKY" : "EFFECTIVE";
 		}
 		
-		add(riskContents, BorderLayout.CENTER);
+		JScrollPane scroll = new JScrollPane(new JTable(body, HEADER));
 		
-		JPanel addForm = new JPanel();
-		addForm.setLayout(new FlowLayout());
-		
-		JTextField input = new JTextField(INPUT_MAX_SIZE);
-		input.setEditable(true);
-		
-		addForm.add(input);
-		
-		JButton addBtn = new JButton("Add");
-		
-		addForm.add(addBtn);
+		add(scroll);
 		
 		setVisible(true);
-	}
-	
-	public Ages parseAge(int age) {
-		Ages ages;
-		
-		switch(age/10) {
-		case 0: ages = Ages.Infant;
-			break;
-		case 1: ages = Ages.Teen;
-			break;
-		case 2:case 3: ages = Ages.Youth;
-			break;
-		case 4:case 5: ages = Ages.MiddleAge;
-			break;
-		case 6:case 7: ages = Ages.ElderAge;
-			break;
-		default: ages = Ages.OldAge;
-			break;
-		}
-		
-		return ages;
-	}
-
-	@Override
-	public double calcRisk(Ages age, HumanRaces humanRace) {
-		return RISK_FOR_AGE[age.ordinal()] * RISK_FOR_HUMAN_RACE[humanRace.ordinal()];
-	}
-
-	@Override
-	public double calcProfit(Ages age, HumanRaces humanRace) {
-		return 0;
-	}
-
-	@Override
-	public boolean canInoculation(Ages age, HumanRaces humanRace) {
-		return calcRisk(age, humanRace) < calcProfit(age, humanRace);
 	}
 }
--- vaccineView/InventoryContents.java
+++ vaccineView/InventoryContents.java
@@ -5,35 +5,29 @@ import javax.swing.*;
 import vaccine.*;
 
 public class InventoryContents extends JPanel {
-	public static final int TEXTAREA_ROW = 20;
-	public static final int TEXTAREA_COL = 15;
+	public static final String[] HEADER = {"Number", "Capacity", "Produce Date", "Expire Date"};
 	
 	private Vaccine vaccine;
-	private JTextArea inventory;
+	private String[][] body;
 	
 	public InventoryContents(Vaccine vaccine) {
 		super();
-		
 		this.vaccine = vaccine;
-		
+		this.body = new String[vaccine.getInventory().size()][HEADER.length];
 		setLayout(new BorderLayout());
 		
-		inventory = new JTextArea(TEXTAREA_ROW, TEXTAREA_COL);
-		inventory.setEditable(false);
-		updateIventory();
+		for(int i=0 ; i<body.length ; i++) {
+			body[i][0] = vaccine.getInventory().get(i).getVaccineNum() + "";
+			body[i][1] = vaccine.getInventory().get(i).getCapacity() + "";
+			body[i][2] = vaccine.getInventory().get(i).getProductDate().toString();
+			body[i][3] = vaccine.getInventory().get(i).getExpireDate().toString();
+		}
 		
-		JScrollPane scroll = new JScrollPane(inventory);
+		JScrollPane scroll = new JScrollPane(new JTable(body, HEADER));
 		scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		
 		add(scroll);
 		
 		setVisible(true);
-	}
-	
-	public void updateIventory() {
-		String res = "";
-		for(VaccineProduct vp : vaccine.getInventory())
-			res += vp.toString() +"\n";
-		inventory.setText(res);
 	}
 }
