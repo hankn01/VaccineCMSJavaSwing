46433f59d9982407bb98e4ba4d83580f5c1c83bc
Kangmin Han <62194473+hankn01@users.noreply.github.com>
2021-05-27 15:44:56 +0900

    VaccineMainMenuButton Implemented

--- country/CountryScreen.java
+++ country/CountryScreen.java
@@ -50,18 +50,21 @@ public class CountryScreen extends JFrame{
 		JButton LocalStock = new JButton("지역 백신 관리");
 		
 		
-		JButton setBudget = new JButton("국가 예산 설정");
+		JButton setCp = new JButton("백신회사별 백신 분량 및 유통기한 관리");
+		
+		
 		
 		NationStock.addActionListener(new ListenerClass());
 		NationStock.setActionCommand("국가관리");
 		LocalStock.addActionListener(new ListenerClass());
 		LocalStock.setActionCommand("지역관리");
-		
+		setCp.addActionListener(new ListenerClass());
+		setCp.setActionCommand("회사별분량");
 		CountryWindow.add(CountryName,BorderLayout.NORTH);
 		CountryWindow.add(CurrentBudget,BorderLayout.CENTER);
 		downPanel.add(NationStock,BorderLayout.WEST);
 		downPanel.add(LocalStock,BorderLayout.CENTER);
-		downPanel.add(setBudget,BorderLayout.EAST);
+		downPanel.add(setCp,BorderLayout.EAST);
 		
 		CountryWindow.add(downPanel,BorderLayout.SOUTH);
 		
@@ -71,27 +74,6 @@ public class CountryScreen extends JFrame{
 		
 		
 		
-		//stub code
-		
-		double cost = 100;
-		String RNAName = "MRNA-1273";
-		double protectionRate = 90;
-		double coldChainDegree = -70;
-		int inoculationTime = 2;
-		SideEffects[] sideEffects = {SideEffects.Pain, SideEffects.Blush, SideEffects.Swelling, SideEffects.Fatigue, SideEffects.Headache};
-		
-		double sideEffectRisk = 0.5;
-		
-		ArrayList<VaccineProduct> inventory = new ArrayList<VaccineProduct>(0);
-		inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,13), new Date(2021,5,20)));
-		inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,14), new Date(2021,5,21)));
-		inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,15), new Date(2021,5,22)));
-		inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,16), new Date(2021,5,23)));
-		inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,17), new Date(2021,5,24)));
-		
-		VaccineMainMenu vmm = new VaccineMainMenu(new Moderna(
-				cost, protectionRate, coldChainDegree, inoculationTime, sideEffectRisk, sideEffects, inventory, RNAName
-		));
 		
 		
 		
@@ -110,9 +92,37 @@ public class CountryScreen extends JFrame{
 			{
 				LocalStockScreen lsc = new LocalStockScreen();
 			}
+			else if(e.getActionCommand().equals("회사별분량"))
+			{
+				//stub code
+				
+				double cost = 100;
+				String RNAName = "MRNA-1273";
+				double protectionRate = 90;
+				double coldChainDegree = -70;
+				int inoculationTime = 2;
+				SideEffects[] sideEffects = {SideEffects.Pain, SideEffects.Blush, SideEffects.Swelling, SideEffects.Fatigue, SideEffects.Headache};
+				
+				double sideEffectRisk = 0.5;
+				
+				ArrayList<VaccineProduct> inventory = new ArrayList<VaccineProduct>(0);
+				inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,13), new Date(2021,5,20)));
+				inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,14), new Date(2021,5,21)));
+				inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,15), new Date(2021,5,22)));
+				inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,16), new Date(2021,5,23)));
+				inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,17), new Date(2021,5,24)));
+				
+				VaccineMainMenu vmm = new VaccineMainMenu(new Moderna(
+						cost, protectionRate, coldChainDegree, inoculationTime, sideEffectRisk, sideEffects, inventory, RNAName
+				));
+				
+				
+				
+				
+			}
 			else
 			{
-				JOptionPane.showMessageDialog(null, "프로그램 동작 오류 (에러코드 201)");
+				JOptionPane.showMessageDialog(null, "프로그램 동작 오류  (에러코드 201)");
 			}
 		}
 		
