18f24a1db97809e3d60095de1feb410686899900
Kangmin Han <62194473+hankn01@users.noreply.github.com>
2021-05-30 20:28:39 +0900

    전역 변수 Class 구현

--- country/GUISelectCompany.java
+++ country/GUISelectCompany.java
@@ -8,9 +8,13 @@ import vaccine.Date;
 import vaccine.Moderna;
 import vaccine.Pfizer;
 import vaccine.VaccineProduct;
+import vaccine.VaccineType;
+import vaccine.Yansen;
 import vaccine.SideEffect.SideEffects;
+import vaccineView.VaccineAdd;
 import vaccineView.VaccineMainMenu;
 
+import javax.management.remote.SubjectDelegationPermission;
 import javax.swing.JButton;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
@@ -21,7 +25,23 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 
-public class GUISelectCompany extends JFrame implements ActionListener{
+public class GUISelectCompany extends JFrame implements ActionListener, VaccineType{
+	
+	
+	
+	SideEffects[] sf = {SideEffects.Pain, SideEffects.Blush, SideEffects.Swelling, SideEffects.Fatigue, SideEffects.Headache};
+	AZ AZdata = null;
+	ArrayList<VaccineProduct> AZinventory = null;
+	Pfizer Pfizerdata = null;
+	ArrayList<VaccineProduct> Pfizerinventory = null;
+	ChadOx1 ChadOx1data = null;
+	ArrayList<VaccineProduct> ChadOx1inventory = null;
+	Moderna Modernadata = null;
+	ArrayList<VaccineProduct> Modernainventory = null;
+	Yansen Yansendata = null;
+	ArrayList<VaccineProduct> Yanseninventory = null;
+	
+	
 	public GUISelectCompany()
 	{
 		super("회사 선택");
@@ -36,6 +56,25 @@ public class GUISelectCompany extends JFrame implements ActionListener{
 		
 		
 		
+
+		
+		AZinventory = new ArrayList<VaccineProduct>(0);
+		
+		AZinventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,13), new Date(2021,5,20)));
+		
+		Pfizerinventory = new ArrayList<VaccineProduct>(0);
+		
+		Pfizerinventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,13), new Date(2021,5,20)));
+		
+				
+		ChadOx1inventory = new ArrayList<VaccineProduct>(0);
+		ChadOx1inventory.add(new VaccineProduct(20,6.5, new Date(2021,5,13),new Date(2021,5,20)));
+		
+		Modernainventory = new ArrayList<VaccineProduct>(0);
+		Modernainventory.add(new VaccineProduct(20,6.5,new Date(2021,5,13), new Date(2021,5,20)));
+		
+		Yanseninventory = new ArrayList<VaccineProduct>(0);
+		Yanseninventory.add(new VaccineProduct(20,6.5,new Date(2021,5,13),new Date(2021,5,20)));
 		
 		
 		JButton AZ = new JButton("AstraZeneca(AZ)");
@@ -76,74 +115,28 @@ public class GUISelectCompany extends JFrame implements ActionListener{
 	{
 		if(e.getActionCommand().equals("아스트라"))
 		{
-			double cost = 100;
-			String RNAName = "MRNA-1273";
-			double protectionRate = 90;
-			double coldChainDegree = -70;
-			int inoculationTime = 2;
-			SideEffects[] sideEffects = {SideEffects.Pain, SideEffects.Blush, SideEffects.Swelling, SideEffects.Fatigue, SideEffects.Headache};
 			
-			double sideEffectRisk = 0.5;
+			AZdata = new AZ(VaccineType.cost[2],90,VaccineType.coldChainDegree[2],VaccineType.inoculationTime,VaccineType.sideEffectRisk[2],sf,AZinventory,VaccineType.RNAName[2]);
+
 			
-			ArrayList<VaccineProduct> inventory = new ArrayList<VaccineProduct>(0);
-			inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,13), new Date(2021,5,20)));
-			inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,14), new Date(2021,5,21)));
-			inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,15), new Date(2021,5,22)));
-			inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,16), new Date(2021,5,23)));
-			inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,17), new Date(2021,5,24)));
-			
-			VaccineMainMenu vmm = new VaccineMainMenu(new AZ(
-					cost, protectionRate, coldChainDegree, inoculationTime, sideEffectRisk, sideEffects, inventory, RNAName
-			));
+			VaccineMainMenu vmm = new VaccineMainMenu(AZdata, null);
 			
 			
 		}
 		else if(e.getActionCommand().equals("차드"))
 		{
-			double cost = 100;
-			String[] DNAName = {"MRNA-1273","b"};
-			double protectionRate = 90;
-			double coldChainDegree = -70;
-			int inoculationTime = 2;
-			SideEffects[] sideEffects = {SideEffects.Pain, SideEffects.Blush, SideEffects.Swelling, SideEffects.Fatigue, SideEffects.Headache};
-			
-			double sideEffectRisk = 0.5;
-			
-			ArrayList<VaccineProduct> inventory = new ArrayList<VaccineProduct>(0);
-			inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,13), new Date(2021,5,20)));
-			inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,14), new Date(2021,5,21)));
-			inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,15), new Date(2021,5,22)));
-			inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,16), new Date(2021,5,23)));
-			inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,17), new Date(2021,5,24)));
-			
-			VaccineMainMenu vmm = new VaccineMainMenu(new ChadOx1(cost, protectionRate, coldChainDegree, inoculationTime, sideEffectRisk, sideEffects, inventory, DNAName, true, new Date(2021,1,1)));
-			
-			
-			
+			String[] temp = {"a"};
+			ChadOx1data = new ChadOx1(VaccineType.cost[4],90,VaccineType.coldChainDegree[4],VaccineType.inoculationTime,VaccineType.sideEffectRisk[4],sf,ChadOx1inventory,temp,false,new Date(2020,1,1));
+			VaccineMainMenu vmm = new VaccineMainMenu(ChadOx1data, null);
 			
 		}
 		else if(e.getActionCommand().equals("모더나"))
 		{
 
-			double cost = 100;
-			String RNAName = "MRNA-1273";
-			double protectionRate = 90;
-			double coldChainDegree = -70;
-			int inoculationTime = 2;
-			SideEffects[] sideEffects = {SideEffects.Pain, SideEffects.Blush, SideEffects.Swelling, SideEffects.Fatigue, SideEffects.Headache};
+			Modernadata = new Moderna(VaccineType.cost[0],90,VaccineType.coldChainDegree[0],VaccineType.inoculationTime,VaccineType.sideEffectRisk[0],sf,AZinventory,VaccineType.RNAName[0]);
 			
-			double sideEffectRisk = 0.5;
 			
-			ArrayList<VaccineProduct> inventory = new ArrayList<VaccineProduct>(0);
-			inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,13), new Date(2021,5,20)));
-			inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,14), new Date(2021,5,21)));
-			inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,15), new Date(2021,5,22)));
-			inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,16), new Date(2021,5,23)));
-			inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,17), new Date(2021,5,24)));
-			
-			VaccineMainMenu vmm = new VaccineMainMenu(new Moderna(
-					cost, protectionRate, coldChainDegree, inoculationTime, sideEffectRisk, sideEffects, inventory, RNAName
-			));
+			VaccineMainMenu vmm = new VaccineMainMenu(Modernadata,null);
 			
 			
 			
@@ -151,50 +144,20 @@ public class GUISelectCompany extends JFrame implements ActionListener{
 		else if(e.getActionCommand().equals("화이자"))
 		{
 			
-			double cost = 100;
-			String RNAName = "VVCT";
-			double protectionRate = 90;
-			double coldChainDegree = -70;
-			int inoculationTime = 2;
-			SideEffects[] sideEffects = {SideEffects.Pain, SideEffects.Blush, SideEffects.Swelling, SideEffects.Fatigue, SideEffects.Headache};
+			Pfizerdata = new Pfizer(this.cost[1],90,VaccineType.coldChainDegree[1], VaccineType.inoculationTime,VaccineType.sideEffectRisk[1],sf,Pfizerinventory,VaccineType.RNAName[1]);
+
 			
-			double sideEffectRisk = 0.5;
-			
-			ArrayList<VaccineProduct> inventory = new ArrayList<VaccineProduct>(0);
-			inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,13), new Date(2021,5,20)));
-			inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,14), new Date(2021,5,21)));
-			inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,15), new Date(2021,5,22)));
-			inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,16), new Date(2021,5,23)));
-			inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,17), new Date(2021,5,24)));
-			
-			VaccineMainMenu vmm = new VaccineMainMenu(new Pfizer(
-					cost, protectionRate, coldChainDegree, inoculationTime, sideEffectRisk, sideEffects, inventory, RNAName
-			));
+			VaccineMainMenu vmm = new VaccineMainMenu(Pfizerdata,null);
 			
 			
 			
 		}
 		else if(e.getActionCommand().equals("얀센"))
 		{
-			double cost = 100;
-			String RNAName = "VVCT";
-			double protectionRate = 90;
-			double coldChainDegree = -70;
-			int inoculationTime = 2;
-			SideEffects[] sideEffects = {SideEffects.Pain, SideEffects.Blush, SideEffects.Swelling, SideEffects.Fatigue, SideEffects.Headache};
+			Yansendata = new Yansen(this.cost[3],90,VaccineType.coldChainDegree[3], VaccineType.inoculationTime,VaccineType.sideEffectRisk[3],sf,Pfizerinventory,VaccineType.RNAName[3]);
 			
-			double sideEffectRisk = 0.5;
 			
-			ArrayList<VaccineProduct> inventory = new ArrayList<VaccineProduct>(0);
-			inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,13), new Date(2021,5,20)));
-			inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,14), new Date(2021,5,21)));
-			inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,15), new Date(2021,5,22)));
-			inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,16), new Date(2021,5,23)));
-			inventory.add(new VaccineProduct(20, 6.5, new Date(2021,5,17), new Date(2021,5,24)));
-			
-			VaccineMainMenu vmm = new VaccineMainMenu(new Pfizer(
-					cost, protectionRate, coldChainDegree, inoculationTime, sideEffectRisk, sideEffects, inventory, RNAName
-			));
+			VaccineMainMenu vmm = new VaccineMainMenu(Yansendata, null);
 			
 		}
 		else if(e.getActionCommand().equals("기타"))
